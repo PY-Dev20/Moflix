8782bd32e1c36681166c2d0fdd334fe2eeca049b
PY-Dev20 <aboodi.nour@gmail.com>
2022-12-30 16:36:49 +0400

    store and cart

--- store/models.py
+++ store/models.py
@@ -15,9 +15,9 @@ class Product(models.Model):
     created_date    = models.DateTimeField(auto_now_add=True)
     modified_date   = models.DateTimeField(auto_now=True)
     year = models.PositiveIntegerField()  
-    rated = models.CharField(max_length=50)
+    rated = models.CharField(max_length=200)
     runtime = models.PositiveIntegerField()
-    genre = models.CharField(max_length=100)
+    genre = models.CharField(max_length=200)
     size = models.DecimalField(max_digits=3, decimal_places=1)
     
     
@@ -26,3 +26,27 @@ class Product(models.Model):
     
     def __str__(self):
         return self.product_name
+    
+class VariationManager(models.Manager):
+    def colors(self):
+        return super(VariationManager, self).filter(variation_category='color', is_active=True)
+
+    def sizes(self):
+        return super(VariationManager, self).filter(variation_category='size', is_active=True)
+
+variation_category_choice = (
+    ('color', 'color'),
+    ('size', 'size'),
+)
+
+class Variation(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    variation_category = models.CharField(max_length=100, choices=variation_category_choice)
+    variation_value     = models.CharField(max_length=100)
+    is_active           = models.BooleanField(default=True)
+    created_date        = models.DateTimeField(auto_now=True)
+
+    objects = VariationManager()
+
+    def __str__(self):
+        return self.variation_value
